d7e0745d1cecd2b10d564c65e89a572f3a9b2815
p11co <39766228+P11co@users.noreply.github.com>
2026-06-16 12:19:20 +0900

    update(CORS): allow localhost port

--- backend/src/konopro_backend/config.py
+++ backend/src/konopro_backend/config.py
@@ -32,6 +32,8 @@ class BackendSettings(BaseSettings):
         "http://localhost:5173,"
         "http://127.0.0.1:5500,"
         "http://localhost:5500,"
+        "http://127.0.0.1:8765,"
+        "http://localhost:8765,"
         "http://127.0.0.1:8000,"
         "http://localhost:8000"
     )
